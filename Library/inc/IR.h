5835a82674cc14e217e89b3b20651fbc38b1cb27
hoangtamAT <hoangtamhc22@gmail.com>
2017-11-13 20:30:50 +0700

    Edit Final

--- Library/inc/IR.h
+++ Library/inc/IR.h
@@ -8,7 +8,7 @@
 #define dl50ms	14100
 
 //#define IR_DATA	GPIO_ReadInputPin(GPIOD,GPIO_PIN_4)
-#define IR_DATA	GPIO_ReadInputData(GPIOD)
+#define IR_DATA	GPIO_ReadInputData(GPIOB) //PB4
 uint8_t IR_CheckStart();
 uint8_t IR_GetCode(void);
 void delay_us(int n);
